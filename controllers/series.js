5f9edd0f04c41440cae38fddfb9c08c949879601
Philippe <philippe.bjj@gmail.com>
2020-09-10 12:22:14 +0100

    labels adjustments

--- controllers/series.js
+++ controllers/series.js
@@ -1,57 +1,60 @@
 const labels = [
-  { id: 'to-watch', name: 'Para Assistir' },
-  { id: 'watching', name: 'Assistindo' },
-  { id: 'watched', name: 'Assistido' }
-]
+  { id: "to-watch", name: "Ver Mais Tarde" },
+  { id: "watching", name: "A Ver" },
+  { id: "watched", name: "Visto" },
+];
 
 const index = async ({ Serie }, req, res) => {
-  const docs = await Serie.find({})
-  res.render('series/index', { series: docs, labels })
-}
+  const docs = await Serie.find({});
+  res.render("series/index", { series: docs, labels });
+};
 
 const novaProcess = async ({ Serie }, req, res) => {
-  const serie = new Serie(req.body)
+  const serie = new Serie(req.body);
   try {
-    await serie.save()
-    res.redirect('/series')
+    await serie.save();
+    res.redirect("/series");
   } catch (err) {
-    res.redirect('/series/nova', {
-      error: Object.keys(err.errors)
-    })
+    res.redirect("/series/nova", {
+      error: Object.keys(err.errors),
+    });
   }
-}
+};
 
 const novaForm = (req, res) => {
-  res.render('series/nova')
-}
+  res.render("series/nova");
+};
 
 const excluir = async ({ Serie }, req, res) => {
-  await Serie.deleteOne({ _id: req.params.id })
-  res.redirect('/series')
-}
+  await Serie.deleteOne({ _id: req.params.id });
+  res.redirect("/series");
+};
 
 const updateProcess = async ({ Serie }, req, res) => {
-  const serie = await Serie.findOne({ _id: req.params.id })
-  serie.name = req.body.name
-  serie.status = req.body.status
-  await serie.save()
-  res.redirect('/series')
-}
+  const serie = await Serie.findOne({ _id: req.params.id });
+  serie.name = req.body.name;
+  serie.status = req.body.status;
+  await serie.save();
+  res.redirect("/series");
+};
 
 const updateForm = async ({ Serie }, req, res) => {
-  const serie = await Serie.findOne({ _id: req.params.id })
-  res.render('series/update', { serie, labels })
-}
+  const serie = await Serie.findOne({ _id: req.params.id });
+  res.render("series/update", { serie, labels });
+};
 
 const info = async ({ Serie }, req, res) => {
-  const serie = await Serie.findOne({ _id: req.params.id })
-  res.render('series/info', { serie })
-}
+  const serie = await Serie.findOne({ _id: req.params.id });
+  res.render("series/info", { serie });
+};
 
 const addComment = async ({ Serie }, req, res) => {
-  await Serie.updateOne({ _id: req.params.id }, { $push: { comments: req.body.comentario } })
-  res.redirect('/series/info/' + req.params.id)
-}
+  await Serie.updateOne(
+    { _id: req.params.id },
+    { $push: { comments: req.body.comentario } }
+  );
+  res.redirect("/series/info/" + req.params.id);
+};
 
 module.exports = {
   index,
@@ -61,5 +64,5 @@ module.exports = {
   updateForm,
   updateProcess,
   info,
-  addComment
-}
+  addComment,
+};
